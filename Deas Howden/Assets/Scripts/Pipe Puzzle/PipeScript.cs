560da7ff89b7b81ea27d3dbf54ed05f0d07158f7
Ubitroll <stephencartner@hotmail.co.uk>
2019-11-11 23:22:35 +0000

    Pipe Puzzle for Prototype Pitch done

---
+++ Deas Howden/Assets/Scripts/Pipe Puzzle/PipeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeScript : MonoBehaviour
+{
+    public Camera cam;
+
+    private void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If left mouse clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                hit.transform.Rotate(0, 90, 0);
+            }
+        }
+    } 
+}
